07dec975e51b021734a13aeeae8cf576cf9dc732
SpIdYs1337 <113311890+SpIdYs1337@users.noreply.github.com>
2026-09-09 21:46:04 +0300

    fix:megrege

--- uppetit-audit/src/lib/version.ts
+++ uppetit-audit/src/lib/version.ts
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-export const APP_VERSION = "2.9.8";
-=======
-export const APP_VERSION = "2.9.9";
->>>>>>> 207c21055b1ecec9bf160618dcffa38573fe916b
+export const APP_VERSION = "3.0.0";
